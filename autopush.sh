9dd594d4ffcf9b266b05d17da09efd28ed3b0d93
DarthPellegrini <leonardo.pellegrini.silva@outlook.com>
2018-10-06 17:41:44 -0300

    teste

--- autopush.sh
+++ autopush.sh
@@ -4,4 +4,11 @@ git add .
 echo commit message:
 read var
 git commit -m $var
-
+git checkout master
+git pull
+git checkout dev
+git merge dev master
+git checkout master
+git merge master dev
+git push
+git checkout dev
