9acb301e5ebd67265b4f45489a3b8412ab45190b
DarthPellegrini <leonardo.pellegrini.silva@outlook.com>
2018-10-07 16:31:17 -0300

    endril - bug fix

--- logic.py
+++ logic.py
@@ -137,8 +137,14 @@ def translate(c1, c2, c3, c4):
         if c1[x] == c3[x] and c2[x] == c4[x]:  # sempre que houver uma função faça a linha de comando apontada pelo faça é garantida na tradução
             y = c2[x] - 1
             seq.append(y)
-            if c1[y] == c3[y] and c2[y] == c4[y]:
-                dicionario[y] = len(seq)+1
+            try:
+                if c1[y] == c3[y] and c2[y] == c4[y]:
+                    if y not in seq:
+                        dicionario[y] = len(seq) + 1
+                    else:
+                        dicionario[y] = c2[y]
+            except:
+                pass
         c1, c2, ignore = ifVerification(x, c1, c2, c3, c4, ignore) # verificação do campo true
         c3, c4, ignore = ifVerification(x, c3, c4, c1, c2, ignore) # verificação do campo false
         x += 1
